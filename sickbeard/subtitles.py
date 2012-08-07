256ea99ae19203b1d5b15d4cd1c49b10cbf586c7
nanda <nanda@firdau.si>
2012-08-07 23:11:48 +0200

    Fix date and video path.

--- sickbeard/subtitles.py
+++ sickbeard/subtitles.py
@@ -118,14 +118,18 @@ class SubtitlesFinder():
             if not ek.ek(os.path.isfile, epToSub['location']):
                 logger.log('Episode file does not exist, cannot download subtitles for episode %dx%d of show %s' % (epToSub['season'], epToSub['episode'], epToSub['show_name']), logger.DEBUG)
                 continue
+            try:
+                lastsearch = datetime.datetime.strptime(epToSub['lastsearch'], '%Y-%m-%d %H:%M:%S')
+            except:
+                lastsearch = datetime.datetime.strptime(epToSub['lastsearch'], '%Y-%m-%d %H:%M:%S.%f')
             # Old shows rule
-            if epToSub['airdate_daydiff'] > 7 and epToSub['searchcount'] < 2 and (now - datetime.datetime.strptime(epToSub['lastsearch'], '%Y-%m-%d %H:%M:%S.%f')) > datetime.timedelta(hours=rules['old'][epToSub['searchcount']]):
+            if epToSub['airdate_daydiff'] > 7 and epToSub['searchcount'] < 2 and (now - lastsearch) > datetime.timedelta(hours=rules['old'][epToSub['searchcount']]):
                 logger.log('Downloading subtitles for episode %dx%d of show %s' % (epToSub['season'], epToSub['episode'], epToSub['show_name']), logger.DEBUG)
                 locations.append(epToSub['location'])
                 toRefresh.append((epToSub['showid'], epToSub['season'], epToSub['episode']))
                 continue
             # Recent shows rule
-            if epToSub['airdate_daydiff'] <= 7 and epToSub['searchcount'] < 7 and (now - datetime.datetime.strptime(epToSub['lastsearch'], '%Y-%m-%d %H:%M:%S.%f')) > datetime.timedelta(hours=rules['new'][epToSub['searchcount']]):
+            if epToSub['airdate_daydiff'] <= 7 and epToSub['searchcount'] < 7 and (now - lastsearch) > datetime.timedelta(hours=rules['new'][epToSub['searchcount']]):
                 logger.log('Downloading subtitles for episode %dx%d of show %s' % (epToSub['season'], epToSub['episode'], epToSub['show_name']), logger.DEBUG)
                 locations.append(epToSub['location'])
                 toRefresh.append((epToSub['showid'], epToSub['season'], epToSub['episode']))
